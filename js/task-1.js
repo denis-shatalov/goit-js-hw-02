3316002985e77fe51d20e25e7a909302f66f9940
den4ik <denis.shatalov95@gmail.com>
2024-11-09 03:29:38 +0200

    hw-js2

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,11 @@
-function makeTransaction(quantity, pricePerDroid) {
-    const totalPrice = quantity * pricePerDroid;
-    const yourOrder = `Your ordered ${quantity} droids worth ${totalPrice} credits!`;
-    return yourOrder;
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+    const orderPrice = quantity * pricePerDroid;
+    if (orderPrice > customerCredits) {return "Insufficient funds!"}
+    else {return `You ordered ${quantity} droids worth ${orderPrice} credits!` }
 }
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
-
+console.log(makeTransaction(5, 3000, 23000))
+console.log(makeTransaction(3, 1000, 15000));
+console.log(makeTransaction(10, 5000, 8000));
+console.log(makeTransaction(8, 2000, 10000));
+console.log(makeTransaction(10, 500, 5000));
 
--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,12 @@
-function getElementWidth(content, padding, border) {
-    const numContent = Number.parseInt(content);
-    const numPadding = Number.parseInt(padding);
-    const numBorder = Number.parseFloat(border);
-    const totalWidth = numContent + (numPadding * 2) + (numBorder * 2);
-    return totalWidth
+function checkForSpam(message) {
+    const result = message.toLowerCase().includes ("spam") || message.toLowerCase().includes ("sale");
+    return result;
 }
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px")); 
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
